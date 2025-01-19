e16a428abb31c662c6a751e0013006ba36d0f567
Ico9409 <hristaka299@gmail.com>
2025-01-19 19:55:11 +0200

    Pathfinder V2.0

--- src/main/java/bg/softuni/pathfinder/web/RouteController.java
+++ src/main/java/bg/softuni/pathfinder/web/RouteController.java
@@ -82,7 +82,6 @@ public class RouteController {
 
         return modelAndView;
     }
-// Changes
     @GetMapping("/routes/{category}")
     public ModelAndView getRoutesByCategory(@PathVariable CategoryType category) {
         String view = "";
