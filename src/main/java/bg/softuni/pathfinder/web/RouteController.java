f6d9831c5655f9c2b4e44fdb15c6ebfffbbd1ee0
Hristo Hristov <hristaka299@gmail.com>
2025-01-21 09:53:23 +0200

    Pathfinder V2.0

--- src/main/java/bg/softuni/pathfinder/web/RouteController.java
+++ src/main/java/bg/softuni/pathfinder/web/RouteController.java
@@ -82,6 +82,7 @@ public class RouteController {
 
         return modelAndView;
     }
+    //
     @GetMapping("/routes/{category}")
     public ModelAndView getRoutesByCategory(@PathVariable CategoryType category) {
         String view = "";
